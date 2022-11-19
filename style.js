17788525dadc86d8e794c572b2faefb25d863768
Wangqin9610 <17864196203@163.com>
2022-11-19 13:00:18 +0800

    '订单页'

---
+++ style.js
@@ -0,0 +1,13 @@
+(function(doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 100 * (clientWidth / 750) + 'px'; // 默认1:1时候是750
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window)
